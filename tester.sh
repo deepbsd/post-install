d78915897319e32162645b22eeb40fef81709d71
David S. Jackson <deepbsd@yahoo.com>
2021-11-09 18:43:32 -0500

    still working on an idea

--- tester.sh
+++ tester.sh
@@ -9,17 +9,18 @@ check_folders(){
 
     folders=$(whiptail --title "Choose directories to copy" --backtitle "CHOOSE DIRECTORIES" --checklist \
     "Choose Folder Options:" 20 78 13 \
-    "adm" " " ON \
-    "dotfiles" " " ON \
-    ".vim" " " ON \
-    "public_html" " " ON \
-    "sounds" " " ON \
-    ".gkrellm2" " " ON \
-    "wallpaper" " " ON \
-    "wallpaper1" " " ON \
-    "bin" " " ON \
-    ".ssh" " " ON \
-    ".gnupg" " " ON \
-    "Music" " " OFF 3>&1 1>&2 2>&3 )
-
+    "$myfolders"
+    3>&1 1>&2 2>&3 )
 }
+    #"adm" " " ON \
+    #"dotfiles" " " ON \
+    #".vim" " " ON \
+    #"public_html" " " ON \
+    #"sounds" " " ON \
+    #".gkrellm2" " " ON \
+    #"wallpaper" " " ON \
+    #"wallpaper1" " " ON \
+    #"bin" " " ON \
+    #".ssh" " " ON \
+    #".gnupg" " " ON \
+    #"Music" " " OFF 3>&1 1>&2 2>&3 )
