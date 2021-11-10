51724ebb35781f29e4c9811643cf0807b4f826e3
David S. Jackson <deepbsd@yahoo.com>
2021-11-10 12:59:46 -0500

    testing with textbox of logfile

--- tester.sh
+++ tester.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+LOGFILE=/tmp/templogfile
+
 password=$(whiptail --backtitle "SUDO PASSWORD CHECKER" --title "Check sudo with auto password" --passwordbox "Please enter your SUDO password" 8 78 3>&1 1>&2 2>&3 )
 
-#echo "$password"
 
-echo "$password" | sudo -u dsj --stdin pacman -Ss x11-ssh-askpass
+echo "$password" | sudo -u dsj --stdin pacman -Ss x11-ssh-askpass >$LOGFILE
+
+whiptail --backtitle "HOW DID WE DO?" --title "Success or not?" --textbox $LOGFILE  40 78
