58ab12df7de669064917faf284937f62249a62dd
David S. Jackson <deepbsd@yahoo.com>
2021-11-09 19:13:55 -0500

    still testing whiptail checklist

--- tester.sh
+++ tester.sh
@@ -6,8 +6,9 @@ FOLDERS=( "adm" "dotfiles" ".vim" "public_html" "sounds" ".gkrellm2" "wallpaper"
 #for f in "${FOLDERS[*]}"; do printf "%s  \" \" ON \ \n" $f; done
 
 check_folders(){
-    myfolders=$(for f in "${FOLDERS[*]}"; do printf "%s  \" \" ON\n" $f; done)
+    myfolders=$(for f in "${FOLDERS[*]}"; do printf "%s  \" \"   ON\n" $f; done)
 
+    # compare outputs between line just below and line 19
     echo -e "$myfolders"
 
     folders=$(whiptail --backtitle "CHOOSE DIRECTORIES" --title "Choose directories to copy" --checklist \
