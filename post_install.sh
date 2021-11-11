5763bc9326df8a6e5d91f3612fd372610e3a1c81
David S. Jackson <deepbsd@yahoo.com>
2021-11-10 20:47:25 -0500

    show textbox after install of nvm

--- post_install.sh
+++ post_install.sh
@@ -214,6 +214,7 @@ install_nvm(){
         mkdir $HOME/.nvm
         [[ -x $(which git &>/dev/null) ]] && cd && git clone https://github.com/nvm-sh/nvm.git .nvm/. &>>$LOGFILE
         [[ -d ~/.nvm ]] && cd ~/.nvm && source nvm.sh && cd  &>>$LOGFILE
+        whiptail --backtitle "NVM INSTALLED" --title "NVM Installation Status" --infobox $LOGFILE 30 78
     else
         term=ANSI  whiptail --backtitle "NVM NOT INSTALLED NOW" --title "NMV not install now" --infobox "Will have to install NVM later on" 10 78
         sleep 2
