c240e70a820578074f1beb4ce744b4d5cc5b7d40
David S. Jackson <deepbsd@yahoo.com>
2021-11-13 16:26:15 -0500

    ssh-service function again

--- post_install.sh
+++ post_install.sh
@@ -147,7 +147,7 @@ ssh_agent_service(){
     Please enter your ssh passphrase: "  10 78
 
     #[[ -f ~/.ssh/id_rsa ]] && eval $(ssh-agent) 2&>/dev/null
-    [[ -f "$SSH_KEY" ]] && eval $(ssh-agent) 2&>>$LOGFILE 
+    [[ -f "$SSH_KEY" ]] && eval $(ssh-agent) &>>$LOGFILE 
 
     password=$(whiptail --backtitle "SUDO PASSWORD CHECKER" --title "Check sudo with auto password" --passwordbox "Please enter your SUDO password" 8 78 3>&1 1>&2 2>&3 )
    
