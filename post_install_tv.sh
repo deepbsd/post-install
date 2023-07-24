455a9083ccda6e572f4a502ef8e17dc580f10ceb
David S. Jackson <deepbsd@yahoo.com>
2023-07-23 23:20:06 -0400

    add timeshift to optional

--- post_install_tv.sh
+++ post_install_tv.sh
@@ -16,7 +16,8 @@ BASICS=( vlc libdvdread libdvdcss libdvdnav gkrellm mlocate fzf )
 DEV_STUFF=( nodejs ruby npm npm-check-updates gvim anaconda )
 FAVES=( gnome-terminal-transparency mate-terminal google-chrome oranchelo-icon-theme-git xcursor-breeze )
 OPTIONAL=( pamac-aur libreoffice-still aisleriot gparted slack-desktop \
-    telegram-desktop galculator atril virtualbox virtualbox-guest-iso virtualbox-host-dkms )
+    telegram-desktop galculator atril timeshift-bin virtualbox virtualbox-guest-iso \
+    virtualbox-host-dkms )
 ## This is the remote hostname (to copy dirs from) make it global for script
 whathost=""
 
